bb7509be5eb780a33e7e30519977a1549d285cf6
Drayce Chen <72390055+cfking94@users.noreply.github.com>
2021-06-14 21:15:18 +0800

    Add files via upload

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,6 +14,7 @@ suite('Functional Tests', function() {
         .query({input: '10L'})
         .end(function(error, res) {
           assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
           assert.equal(res.body.initNum, 10);
           assert.equal(res.body.initUnit, 'L');
           assert.approximately(res.body.returnNum, 2.64172, 0.1)
@@ -65,6 +66,7 @@ suite('Functional Tests', function() {
         .query({input: 'kg'})
         .end(function(error, res) {
           assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
           assert.equal(res.body.initNum, 1);
           assert.equal(res.body.initUnit, 'kg');
           assert.approximately(res.body.returnNum, 2.20462, 0.1);
